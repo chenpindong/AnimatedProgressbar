9ae6fc952a626bfde9e2c8335bf9613982e323cd
chenpindong <cpd2135@163.com>
2019-05-09 16:36:03 +0800

    [+]AnimatedProgressbar

---
+++ Assets/AnimatedProgressbar/Scripts/AnimatedProgressbar.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class AnimatedProgressbar : MonoBehaviour
+{
+    private enum ChangeType
+    {
+        Add, //增加
+        Sub, //减少
+    }
+
+    [SerializeField] private RawImage m_barUI = null;
+    [SerializeField] private float m_fillSpeed = 0;
+    [SerializeField] [Range(0, 1)] private float m_fillAmount = 1;
+    [SerializeField] private bool m_autoPlay = true;
+    [SerializeField] private float m_currentAmount = 0;
+    [SerializeField] private float m_initUvRectWidth = 15;
+    [SerializeField] private float m_speed = 0;
+
+    private bool m_isPlaying = false;
+    private ChangeType m_changeType = ChangeType.Add;
+
+    private float CurrentAmount
+    {
+        get { return m_currentAmount; }
+        set
+        {
+            m_currentAmount = m_changeType == ChangeType.Add ? Mathf.Min(value, m_fillAmount) : Mathf.Max(value, m_fillAmount);
+            var localScale = m_barUI.rectTransform.localScale;
+            localScale.x = m_currentAmount;
+            m_barUI.rectTransform.localScale = localScale;
+
+            var rect = m_barUI.uvRect;
+            rect.width = m_initUvRectWidth * m_currentAmount;
+            m_barUI.uvRect = rect;
+        }
+    }
+
+    public float FillAmount
+    {
+        get
+        {
+            return m_fillAmount;
+        }
+        set
+        {
+            m_changeType = value >= m_currentAmount ? ChangeType.Add : ChangeType.Sub;
+            m_fillAmount = Mathf.Clamp01(value);
+            m_isPlaying = true;
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnValidate()//在Inspector中修改参数值，就会自动调用这个方法
+    {
+        if (Application.isPlaying)
+        {
+            m_changeType = m_fillAmount >= m_currentAmount ? ChangeType.Add : ChangeType.Sub;
+            m_isPlaying = true;
+        }
+        else
+        {
+            CurrentAmount = m_fillAmount;
+        }
+    }
+#endif
+
+    private void Start()
+    {
+        m_changeType = ChangeType.Add;
+        if (m_autoPlay)
+        {
+            m_currentAmount = 0;
+            m_isPlaying = true;
+        }
+        else
+        {
+            CurrentAmount = m_fillAmount;
+            m_isPlaying = false;
+        }
+    }
+
+    private void Update()
+    {
+        var rect = m_barUI.uvRect;
+        rect.x -= m_speed;
+        m_barUI.uvRect = rect;
+
+        if (m_isPlaying)
+        {
+            if (m_changeType == ChangeType.Add)
+            {
+                m_currentAmount += m_fillSpeed;
+                CurrentAmount = m_currentAmount;
+
+                if (m_currentAmount >= m_fillAmount)
+                {
+                    m_isPlaying = false;
+                }
+            }
+            else
+            {
+                m_currentAmount -= m_fillSpeed;
+                CurrentAmount = m_currentAmount;
+
+                if (m_currentAmount <= m_fillAmount)
+                {
+                    m_isPlaying = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/AnimatedProgressbar/Scripts/Editor/AnimatedProgressbarEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(AnimatedProgressbar))]
+[CanEditMultipleObjects]
+public class AnimatedProgressbarEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var barUI = serializedObject.FindProperty("m_barUI");
+        var fillSpeed = serializedObject.FindProperty("m_fillSpeed");
+        var fillAmount = serializedObject.FindProperty("m_fillAmount");
+        var currentAmount = serializedObject.FindProperty("m_currentAmount");
+        var initUvRectWidth = serializedObject.FindProperty("m_initUvRectWidth");
+        var speed = serializedObject.FindProperty("m_speed");
+        var autoPlay = serializedObject.FindProperty("m_autoPlay");
+
+        EditorGUILayout.PropertyField(barUI);
+        EditorGUILayout.PropertyField(fillSpeed);
+        EditorGUILayout.PropertyField(fillAmount);
+        EditorGUILayout.PropertyField(autoPlay);
+        EditorGUILayout.PropertyField(initUvRectWidth);
+        EditorGUILayout.PropertyField(speed);
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.PropertyField(currentAmount);
+        EditorGUI.EndDisabledGroup();
+        serializedObject.ApplyModifiedProperties();
+    }
+}
